673250d07826ba231c8c9017449f27f002738cb0
zenith-kamo <wplacenagai@gmail.com>
2026-09-16 17:53:46 +0900

    feat: 攻撃順序を変更 (Issue #4)

--- src/main/java/com/zenith/udl/util/EntityRemoveUtil.java
+++ src/main/java/com/zenith/udl/util/EntityRemoveUtil.java
@@ -16,23 +16,21 @@ public class EntityRemoveUtil {
             removeEntity(partEntity.getParent(), serverLevel);
             return;
         }
-        entity.remove(Entity.RemovalReason.DISCARDED);
-        entity.setRemoved(Entity.RemovalReason.DISCARDED);
-        entity.gameEvent(GameEvent.ENTITY_DIE);
-        entity.onClientRemoval();
-        entity.invalidateCaps();
-        entity.stopRiding();
-        entity.getPassengers().forEach(Entity::stopRiding);
-        entity.removeVehicle();
-        entity.levelCallback.onRemove(Entity.RemovalReason.DISCARDED);
-        entity.entityData.set(Entity.DATA_POSE, Pose.DYING);
-        entity.onRemovedFromWorld();
+        // other
         entity.setInvulnerable(false);
         entity.invulnerableTime = 0;
         entity.animateHurt(Float.POSITIVE_INFINITY);
-        entity.kill();
-        TargetManager.addKillTarget(entity);
-        serverLevel.getChunkSource().removeEntity(entity);
+        entity.entityData.set(Entity.DATA_POSE, Pose.DYING);
+        entity.gameEvent(GameEvent.ENTITY_DIE);
+        entity.setPose(Pose.DYING);
+        entity.entityData.set(Entity.DATA_POSE, Pose.DYING);
+
+        // passengers
+        entity.stopRiding();
+        entity.getPassengers().forEach(Entity::stopRiding);
+        entity.removeVehicle();
+
+        // pos
         entity.setPos(Float.MAX_VALUE, Float.MIN_VALUE, Float.MAX_VALUE);
         entity.xo = Float.MAX_VALUE;
         entity.yo = Float.MIN_VALUE;
@@ -40,22 +38,33 @@ public class EntityRemoveUtil {
         entity.xOld = Float.MAX_VALUE;
         entity.yOld = Float.MIN_VALUE;
         entity.zOld = Float.MAX_VALUE;
+        // just remove
+        entity.onClientRemoval();
+        entity.invalidateCaps();
+        entity.kill();
+        entity.remove(Entity.RemovalReason.DISCARDED);
+        entity.setRemoved(Entity.RemovalReason.DISCARDED);
+        TargetManager.addKillTarget(entity);
+        entity.levelCallback.onRemove(Entity.RemovalReason.DISCARDED);
+        entity.onRemovedFromWorld();
+        entity.removalReason = Entity.RemovalReason.DISCARDED;
+        serverLevel.getChunkSource().removeEntity(entity);
         if (entity instanceof LivingEntity livingEntity) {
-            livingEntity.deathTime = Integer.MAX_VALUE;
-            livingEntity.isDeadOrDying();
-            livingEntity.setHealth(0.0F);
-            TargetManager.addHealthTarget(livingEntity);
+            // visual
+            livingEntity.setSilent(true);
+            livingEntity.hurtTime = 0;
             livingEntity.canUpdate(false);
-            livingEntity.shouldRender(0,0,0);
+            livingEntity.shouldRender(0, 0, 0);
             livingEntity.handleEntityEvent(EntityEvent.DEATH);
+            livingEntity.deathTime = Integer.MAX_VALUE;
+            // health
+            livingEntity.getCombatTracker().recordDamage(livingEntity.damageSources().generic(), Float.MAX_VALUE);
+            livingEntity.setHealth(0.0F);
             HealthRewriter.entityHealthRewrite(livingEntity, 2);
             HealthRewriter.entityHealthRewrite(livingEntity, 3);
-            livingEntity.deathTime = 20;
-            livingEntity.hurtTime = 0;
-            livingEntity.setSilent(true);
-            livingEntity.getCombatTracker().recordDamage(livingEntity.damageSources().generic(), Float.MAX_VALUE);
+            HealthRewriter.entityHealthRewrite(livingEntity, 4);
+
             livingEntity.die(livingEntity.damageSources().generic());
         }
-
     }
 }
